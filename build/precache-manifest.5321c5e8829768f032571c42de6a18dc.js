d73bc553c7b69cd275bf1d5d47dcff7516d6f98d
carla <carlastnkmp@gmail.com>
2020-02-12 17:11:27 +0200

    build file

---
+++ build/precache-manifest.5321c5e8829768f032571c42de6a18dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf7e8606582c10429140bec31ba0bc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8d9caacf492bce7d012",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "14353d6fbf91a359f49b",
+    "url": "/static/css/main.eb882395.chunk.css"
+  },
+  {
+    "revision": "c8d9caacf492bce7d012",
+    "url": "/static/js/2.0f7093fe.chunk.js"
+  },
+  {
+    "revision": "14353d6fbf91a359f49b",
+    "url": "/static/js/main.34fea3fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
